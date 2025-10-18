e519c2574c46c09dde09dcd7ca33351806b99b37
Chris Wood <chri@Chriss-MacBook-Pro.local>
2025-10-18 18:36:00 +0700

    Add all 12 themes to theme switcher array

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -1,4 +1,18 @@
-// Theme Switcher for L’Art‑de‑la‑Séduction
+const themes = [
+  "default",
+  "maison-de-seduction",
+  "amour-noir",
+  "rouge-seduction",
+  "seductiv-by-lart",
+  "echos-de-soie",
+  "maison-nocturne",
+  "eros-noir",
+  "satin-des-peches",
+  "desir-noir",
+  "desir-brulant",
+  "la-tentatrice",
+  "les-peches-de-la-chair"
+];// Theme Switcher for L’Art‑de‑la‑Séduction
 
 const themes = [
   "default",
